67b77cbe42996449f983e476392320ef688e4d69
himel999 <mahabubulalamofficial@gmail.com>
2021-01-02 00:53:42 +0600

    Number Series by while loop

---
+++ D.loop/A#numberseries.py
@@ -0,0 +1,19 @@
+# Two types of loop in python
+# 1) for loop 2) while loop
+"""
+while loop structure
+
+x = starting value
+while condition:
+    --------
+    --------
+    x = increment/decrement
+
+
+"""
+
+x = int(input("Enter the target value : "))
+
+while x <= 10:
+    print("value is : ", x)
+    x = x+1
