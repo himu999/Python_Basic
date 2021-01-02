eb373f39896f2b4c2cc1be608bf861f55b9292ed
himel999 <mahabubulalamofficial@gmail.com>
2021-01-02 21:17:37 +0600

    Triangle star

---
+++ D.loop/E#pattern1.py
@@ -0,0 +1,10 @@
+row = int(input("Enter the row number : "))
+count = 0
+for i in range(0, row):
+    for j in range(0, row-i-1):
+        print(end=" ")
+
+    count = count + 1
+    for k in range(0, i+count):
+        print("*", end="")
+    print("")
