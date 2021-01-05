a093950b3bc73570ea7088c36e2f686180b12e10
himel999 <mahabubulalamofficial@gmail.com>
2021-01-06 02:17:59 +0600

    HCF

---
+++ E.function/D#4#hcf.py
@@ -0,0 +1,17 @@
+def hcf(a, b):
+    if a > b:
+        smaller = b
+    else:
+        smaller = a
+
+    for i in range(1, smaller+1):
+        if a % i == 0 and b % i == 0:
+            ann = i
+
+    print("HCF : ", ann)
+
+
+k = int(input("First number : "))
+j = int(input("Second number : "))
+
+hcf(k, j)
