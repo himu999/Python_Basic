35003fc5aa8e29b9a2cae3eda5e7b9bf79daf926
himel999 <mahabubulalamofficial@gmail.com>
2021-01-05 12:29:50 +0600

    Fibonacci

---
+++ E.function/C#0#fibonacci.py
@@ -0,0 +1,18 @@
+def fibonacci(n):
+    if n == 0:
+        return 0
+    else:
+        x = 0
+        y = 1
+
+        print(x)
+        print(y)
+
+        for i in range(1, n):
+            z = x+y
+            x = y
+            y = z
+            print(z)
+
+
+fibonacci(int(input("Enter the value of range : ")))
