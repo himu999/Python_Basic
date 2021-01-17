7e329af2529824fb0914c727d31ca2d02b376099
himel999 <mahabubulalamofficial@gmail.com>
2021-01-17 11:38:33 +0600

    String check

---
+++ B1.string/strincheck.py
@@ -0,0 +1,12 @@
+string = "Hi anyone here?"
+
+print("Hi" in string)
+print("hi" in string)
+print("anyone" in string)
+print("Anyone" in string)
+print("Here" in string)
+print("here" in string)
+print("anyone" in string)
+print(" " in string)
+
+
