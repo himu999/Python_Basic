3786fece9bbe7c54607b34c9407507fb3b6f83e5
himel999 <mahabubulalamofficial@gmail.com>
2021-01-17 12:51:55 +0600

    String slicing

---
+++ B1.string/stringslicing.py
@@ -0,0 +1,24 @@
+a = "My name is himel, i like learn new topics everyday"
+b = "Himel Hasan"
+print(b[0:])
+print(b[6:])
+print(b[5:])
+
+print(b[6:7])
+print(b[5:6])
+print(b[5:7])
+print(b[0:11])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
