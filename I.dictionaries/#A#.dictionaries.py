bced827600ce89c37d1339918fc55c878fb94f48
himel999 <mahabubulalamofficial@gmail.com>
2021-01-28 11:03:39 +0600

    Dictionaries

---
+++ I.dictionaries/#A#.dictionaries.py
@@ -0,0 +1,17 @@
+a = {"name": "kind man", "city": "every where", "Born": "with earth"}
+
+print("Element of a : ", a)
+
+print("Length of dictionaries", len(a))
+
+
+b = {1: "name", 2: 'location', 3: 5, 3: "area"}
+print("Element of b : ", b)
+
+print(b[1])
+print(a['name'])
+
+d = {"name": "x", "brand": "nothing", "color": ["red", "green", "blue", "yellow"]}
+print(d)
+
+print(d["color"])
