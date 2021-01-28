181e7caee22603ed1f330d205e60fec2ac0a1eb9
himel999 <mahabubulalamofficial@gmail.com>
2021-01-28 23:12:21 +0600

    Remove item

---
+++ I.dictionaries/#E#remove_item.py
@@ -0,0 +1,18 @@
+a = {"name": "himu", "roll": "1", "class": "eight"}
+a.pop("class")
+print("Element of a : ", a)
+
+b = {"a": "1", "b": "c"}
+b.popitem()
+print("element of b : ", b)
+
+b.clear()
+print(b)
+
+
+'''
+del b
+
+hey don't do this
+'''
+
