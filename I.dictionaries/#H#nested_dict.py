72006d48dc78f10c8b720c74dbbe88d5d2eaeb4c
himel999 <mahabubulalamofficial@gmail.com>
2021-01-29 00:21:26 +0600

    Nested dictionary

---
+++ I.dictionaries/#H#nested_dict.py
@@ -0,0 +1,29 @@
+a = {"child": {
+    "name": "himel",
+    "age": "2"},
+
+    "child1": {
+        "name": "himi",
+        "age": "3"
+    }
+
+}
+
+print(a)
+
+print(list(a.keys()))
+
+child = {
+    "name": "himi",
+    "age": "18+",
+    "relationship status": "single"
+}
+
+child1 = {
+    "name": "himu",
+    "age": "18-",
+    "relationship status": "single"
+}
+
+sad_life = {"False": child, "True": child1}
+print(sad_life)
