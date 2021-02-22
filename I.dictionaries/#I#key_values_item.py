a8cd2f143efdd4df4ab71a2cfec6aa6809427590
himel999 <mahabubulalamofficial@gmail.com>
2021-02-22 22:24:43 +0600

    Revision of dictionary(key, values, items)

---
+++ I.dictionaries/#I#key_values_item.py
@@ -0,0 +1,15 @@
+name = {
+    "my": "Himu", "Her": "Rupa", "City": "Mars", "From": "Heaven"
+}
+
+print("Values of dictionary :", end=" ")
+
+for i in name.values():
+    print(i, end=", ")
+print()
+print("Keys of dictionary :", end=" ")
+for j in name.keys():
+    print(j, end=", ")
+print("Items of dictionary :", end=" ")
+for k in  name.items():
+    print(k, end=", ")
