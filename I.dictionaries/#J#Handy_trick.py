255dbaa5f38b39f154839a09af1e5641729f1f96
himel999 <mahabubulalamofficial@gmail.com>
2021-02-22 23:01:44 +0600

    Handy trick. here we catch "keys" and "values" at a time.

--- I.dictionaries/#J#Handy_trick.py
+++ I.dictionaries/#J#Handy_trick.py
@@ -2,3 +2,8 @@ a = {"name": "himu", "roll": "1", "class": "eight"}
 
 for j, k in a.items():
     print("Keys : "+j+" | Values : "+k)
+
+print("name" in a)
+print("1" in a)
+print("a" in a.keys())
+print("a" in a.values())
